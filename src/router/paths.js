8c790728125e2dbcfa8475ae1423d28747b5ab66
MelisaArce <melisaamelia.arce@gmail.com>
2024-06-30 17:50:34 -0300

    deploy

--- src/router/paths.js
+++ src/router/paths.js
@@ -1,9 +1,8 @@
 export default {
-  ROOT: "/",
-  PRODUCTS: "/products",
-  RECETAS: "/recetas",
-  CART: "/cart",
-  CHECKOUT: "/checkout",
-  GRATITUDE: "/gratitude",
-  ID: "/:id",
+  ROOT: "<%= BASE_URL %>",
+  PRODUCTS: "<%= BASE_URL %>/products",
+  CART: "<%= BASE_URL %>/cart",
+  CHECKOUT: "<%= BASE_URL %>/checkout",
+  GRATITUDE: "<%= BASE_URL %>/gratitude",
+  ID: "<%= BASE_URL %>/:id",
 };
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH : '/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
